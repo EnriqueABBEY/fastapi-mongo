59dca36dedd61edc8f7a20fd69f84e83f6bc4584
Georges ABBEY <abbeyenrique06@gmail.com>
2024-11-30 15:05:35 +0000

    deployment on vercel config

--- src/database.py
+++ src/database.py
@@ -3,7 +3,8 @@ from pymongo import MongoClient
 import os
 
 # Remplacez par l'URI de votre base de données
-MONGODB_URI = os.getenv("MONGODB_URI", "mongodb://app_mongo:27017/")
+# MONGODB_URI = os.getenv("MONGODB_URI", "mongodb://app_mongo:27017/")
+MONGODB_URI = os.getenv("MONGODB_URI", "mongodb+srv://abbeyenrique06:bmCXzNRqiodG0Ov7@cluster0.txag0.mongodb.net/")
 DATABASE_NAME = os.getenv("DATABASE_NAME", "pets")
 
 client = MongoClient(MONGODB_URI)
